9ac6b6e9a1a5dee37dc9717d1b36103504d3f72d
lucaMura03 <154601391+lucaMura03@users.noreply.github.com>
2024-10-18 11:26:58 +0200

    Update exercises.ml

--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -16,17 +16,60 @@ let todo : grammar =
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
+let zero_n_one_n : grammar = 
+        {
+        symbols = [ S ];
+        terminals = [ '0'; '1' ];
+        productions =
+          [
+            S --> "0S1";
+
+            S --> "";
+          ];
+        start = S;
+      }
+
+  
 
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
-
+let palindromes : grammar =
+  {
+  symbols = [ S ];
+  terminals = [ '0'; '1' ];
+  productions =
+    [
+      S --> "0S0";
+      S --> "1S1";
+      S --> "0";
+      S --> "1";
+      S --> "";
+    ];
+  start = S;
+}
 
 (* #### Exercise 3, medium (balanced_parentheses)*)
-let balanced_parentheses : grammar = todo
-
-
+let balanced_parentheses : grammar =  {
+  symbols = [ S, A];
+  terminals = [ '{'; '}'; '('; ')'; '['; ']' ];
+  productions =
+    [
+      S --> "{}";
+      S --> "[]";
+      S --> "()";
+      S --> "(S";
+      S --> ")S[";
+      S --> "]S[";
+      S --> "]A(";
+      S --> "}A[";
+      S --> "}S(";
+      S --> ")S(";
+      S --> "]A[";
+      S --> "}S{";
+      S --> "";
+    ];
+  start = S;
+}
 (* #### Exercise 4, hard (same_amount)
 
    Hint 1: you can use 'a' and 'b' for terminals.
